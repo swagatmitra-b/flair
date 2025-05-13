1547e526f44520666f6a04e8ee9b90beb47d6cbd
Debasish-RS3655 <debashishburagohain000@gmail.com>
2025-05-13 16:45:22 +0530

    access control allow origin all enabled

--- repository_manager/backend/src/app.ts
+++ repository_manager/backend/src/app.ts
@@ -18,6 +18,11 @@ const app = express();
 
 // Middleware
 app.use(morgan('combined'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+})
 app.use(cors());
 app.use(express.json());
 
