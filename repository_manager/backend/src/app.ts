77c254b4947dfbb9d5044aef56c474ebe9f9dabf
Debasish-RS3655 <debashishburagohain000@gmail.com>
2025-05-15 21:03:55 +0530

    request entity limit exceeded to 10mb

--- repository_manager/backend/src/app.ts
+++ repository_manager/backend/src/app.ts
@@ -24,7 +24,11 @@ app.use((req, res, next) => {
     next();
 })
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ 
+  limit: '10mb', 
+  extended: true 
+}));
 
 // trust the proxy
 app.set('trust proxy', true);
