0412074475babac722045816edb86f8929e774c8
Debasish-RS3655 <debashishburagohain000@gmail.com>
2025-05-15 18:28:04 +0530

    added missing next() function in request set headers

--- repository_manager/backend/build/src/app.js
+++ repository_manager/backend/build/src/app.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 app.use(cors());
 app.use(express.json());
@@ -38,6 +39,6 @@ app.all('*', (req, res, next) => {
     res.status(404).send({ error: '404 Not Found' });
 });
 const PORT = 4000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
--- repository_manager/backend/src/app.ts
+++ repository_manager/backend/src/app.ts
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 })
 app.use(cors());
 app.use(express.json());
